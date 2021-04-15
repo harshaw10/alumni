62a91c3d5f8474f5ab3c16202b1856a9bd006051
harshaw10 <82546277+harshaw10@users.noreply.github.com>
2021-04-15 03:03:42 -0400

    Add files via upload

---
+++ alumni-test/src/test/java/alumni/project/Openurl.java
@@ -0,0 +1,52 @@
+package alumni.project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Openurl {
+	
+	WebDriver driver = new SafariDriver();
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+
+	public void ClickOnUseCases() throws InterruptedException
+	{
+		
+		driver.get("https://www.aumni.fund");
+		driver.manage().window().maximize();
+		WebElement usecases = driver.findElement(By.id("w-dropdown-toggle-1"));
+		usecases.click();
+		WebElement Unlock = driver.findElement(By.xpath("//*[@id=\"w-dropdown-list-1\"]/div/a[1]"));
+		Unlock.click();
+		Thread.sleep(3000);
+		driver.close();
+		
+	}
+	
+	public void ClickOnAccelerate() throws InterruptedException
+	{
+		
+		driver.get("https://www.aumni.fund");
+		driver.manage().window().maximize();
+		WebElement usecases = driver.findElement(By.id("w-dropdown-toggle-1"));
+		usecases.click();
+		WebElement Accelerate = driver.findElement(By.xpath("//*[@id=\"w-dropdown-list-1\"]/div/a[2]"));
+		Accelerate.click();
+		Thread.sleep(3000);
+		var firstName = driver.findElement(By.id("first-name"));
+		firstName.sendKeys("John");
+		var lastName = driver.findElement(By.id("last-name"));
+		lastName.sendKeys("Smith");
+		var email = driver.findElement(By.id("email"));
+		email.sendKeys("johnsmith@gmail.com");
+		var company = driver.findElement(By.id("company"));
+		company.sendKeys("PetSmart");
+		var submit = driver.findElement(By.xpath("//*[@id=\"form\"]/div[3]/div/input"));
+		submit.submit();
+		driver.close();
+	}
+	
+}
